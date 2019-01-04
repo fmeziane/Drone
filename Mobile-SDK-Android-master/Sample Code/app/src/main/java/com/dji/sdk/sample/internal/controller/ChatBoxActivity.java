3a9ac60d39c097bf53deccd3e0baf178fd36123a
fmeziane <farid.meziane@live.fr>
2019-01-04 16:48:17 +0100

    ajout nouvelles fonctions up down forward backward left right yaw

--- Mobile-SDK-Android-master/Sample Code/app/src/main/java/com/dji/sdk/sample/internal/controller/ChatBoxActivity.java
+++ Mobile-SDK-Android-master/Sample Code/app/src/main/java/com/dji/sdk/sample/internal/controller/ChatBoxActivity.java
@@ -1,16 +1,22 @@
 package com.dji.sdk.sample.internal.controller;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.dji.sdk.sample.R;
+import com.dji.sdk.sample.internal.OnScreenJoystickListener;
 import com.dji.sdk.sample.internal.utils.DialogUtils;
 import com.dji.sdk.sample.internal.utils.ModuleVerificationUtil;
+import com.dji.sdk.sample.internal.utils.OnScreenJoystick;
+import com.dji.sdk.sample.internal.utils.ToastUtils;
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,12 +24,77 @@ import org.json.JSONObject;
 import java.net.URISyntaxException;
 
 import dji.common.error.DJIError;
+import dji.common.flightcontroller.simulator.InitializationData;
+import dji.common.flightcontroller.simulator.SimulatorState;
+import dji.common.gimbal.Rotation;
+import dji.common.gimbal.RotationMode;
+import dji.common.model.LocationCoordinate2D;
 import dji.common.util.CommonCallbacks;
+import dji.keysdk.FlightControllerKey;
+import dji.keysdk.KeyManager;
 import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.flightcontroller.Simulator;
+import dji.sdk.mobilerc.MobileRemoteController;
+import dji.sdk.products.Aircraft;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+import static dji.common.gimbal.Rotation.NO_ROTATION;
+import static java.lang.Thread.sleep;
+
 
 public class ChatBoxActivity extends AppCompatActivity {
 
     private Socket socket;
+    private OnScreenJoystick screenJoystickLeft;
+    private MobileRemoteController mobileRemoteController;
+    private ToggleButton btnSimulator;
+    private FlightControllerKey isSimulatorActived;
+    private TextView textView;
+
+    public void leftStick(float pX, float pY) {
+        try {
+            mobileRemoteController =
+                    ((Aircraft) DJISampleApplication.getAircraftInstance()).getMobileRemoteController();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        if (Math.abs(pX) < 0.02) {
+            pX = 0;
+        }
+
+        if (Math.abs(pY) < 0.02) {
+            pY = 0;
+        }
+
+        if (mobileRemoteController != null) {
+            mobileRemoteController.setLeftStickHorizontal(pX);
+            mobileRemoteController.setLeftStickVertical(pY);
+        }
+    }
+
+
+    public void rightStick(float pX, float pY) {
+        try {
+            mobileRemoteController =
+                    ((Aircraft) DJISampleApplication.getAircraftInstance()).getMobileRemoteController();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        if (Math.abs(pX) < 0.02) {
+            pX = 0;
+        }
+
+        if (Math.abs(pY) < 0.02) {
+            pY = 0;
+        }
+        if (mobileRemoteController != null) {
+            mobileRemoteController.setRightStickHorizontal(pX);
+            mobileRemoteController.setRightStickVertical(pY);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +102,7 @@ public class ChatBoxActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_chat_box);
 
         try {
-            socket = IO.socket("http://192.168.86.246:3000");
+            socket = IO.socket("http://testserverbmi.westeurope.cloudapp.azure.com:3000");
             socket.connect();
             socket.emit("join", "connecté");
         } catch (URISyntaxException e) {
@@ -81,7 +152,7 @@ public class ChatBoxActivity extends AppCompatActivity {
 
 
                             }
-
+                            // à tester
                             else if (message.equals("landing")) {
 
                                 Toast.makeText(ChatBoxActivity.this, "landing", Toast.LENGTH_SHORT).show();
@@ -96,6 +167,109 @@ public class ChatBoxActivity extends AppCompatActivity {
                                 });
 
                             }
+                            else if (message.equals("forceLand")){
+                                Toast.makeText(ChatBoxActivity.this, "forceLand", Toast.LENGTH_SHORT).show();
+
+                                if (flightController == null) {
+                                    return;
+                                }
+                                flightController.confirmLanding(new CommonCallbacks.CompletionCallback() {
+                                    @Override
+                                    public void onResult(DJIError djiError) {
+                                        DialogUtils.showDialogBasedOnError(ChatBoxActivity.this, djiError);
+                                    }
+                                });
+                            }
+
+
+                            else if (message.equals("yawLeft")){
+
+                                Toast.makeText(ChatBoxActivity.this, "yawLeft", Toast.LENGTH_SHORT).show();
+                                leftStick(-5,0);
+                                Thread.sleep(1000);
+                                leftStick(0,0);
+                            }
+                            else if (message.equals("yawRight")){
+
+                                Toast.makeText(ChatBoxActivity.this, "yawRight", Toast.LENGTH_SHORT).show();
+                                leftStick(5,0);
+                                Thread.sleep(1000);
+                                leftStick(0,0);
+                            }
+
+                            else if (message.equals("yawLeft1")){
+
+                                Toast.makeText(ChatBoxActivity.this, "yawLeft", Toast.LENGTH_SHORT).show();
+                                leftStick(-5,0);
+
+                            }
+                            else if (message.equals("yawRight1")){
+
+                                Toast.makeText(ChatBoxActivity.this, "yawRight", Toast.LENGTH_SHORT).show();
+                                leftStick(5,0);
+
+                            }
+
+                            else if (message.equals("stopYaw")){
+
+                                Toast.makeText(ChatBoxActivity.this, "stopYaw", Toast.LENGTH_SHORT).show();
+                                leftStick(0,0);
+
+                            }
+
+                            else if (message.equals("up")){
+
+                                Toast.makeText(ChatBoxActivity.this, "up", Toast.LENGTH_SHORT).show();
+                                leftStick(0,3);
+                                Thread.sleep(100);
+                                leftStick(0,0);
+                            }
+                            else if (message.equals("down")){
+
+                                Toast.makeText(ChatBoxActivity.this, "down", Toast.LENGTH_SHORT).show();
+                                leftStick(0,-3);
+                                Thread.sleep(100);
+                                leftStick(0,0);
+                            }
+
+                            else if (message.equals("forward")){
+
+                                Toast.makeText(ChatBoxActivity.this, "forward", Toast.LENGTH_SHORT).show();
+                                rightStick(0,5);
+                                Thread.sleep(200);
+                                rightStick(0,0);
+                            }
+                            else if (message.equals("backward")){
+
+                                Toast.makeText(ChatBoxActivity.this, "backward", Toast.LENGTH_SHORT).show();
+                                rightStick(0,-5);
+                                Thread.sleep(200);
+                                rightStick(0,0);
+                            }
+
+                            else if (message.equals("right")){
+
+                                Toast.makeText(ChatBoxActivity.this, "right", Toast.LENGTH_SHORT).show();
+                                rightStick(3,0);
+                                Thread.sleep(100);
+                                rightStick(0,0);
+                            }
+                            else if (message.equals("left")){
+
+                                Toast.makeText(ChatBoxActivity.this, "left", Toast.LENGTH_SHORT).show();
+                                rightStick(-3,0);
+                                Thread.sleep(100);
+                                rightStick(0,0);
+                            }
+
+                            else if (message.equals("stopAll")){
+
+                                Toast.makeText(ChatBoxActivity.this, "stopAll", Toast.LENGTH_SHORT).show();
+                                rightStick(0,0);
+                                rightStick(0,0);
+                            }
+
+                            // à tester
                             else {
 
                                 Toast.makeText(ChatBoxActivity.this, '_'+message+'_', Toast.LENGTH_SHORT).show();
@@ -111,16 +285,55 @@ public class ChatBoxActivity extends AppCompatActivity {
 
                         } catch (JSONException e) {
                             e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
 
-                     }
+                    }
                 });
             }
         });
 
 
     }
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(ChatBoxActivity.this, "onPause", Toast.LENGTH_SHORT).show();
+        //reload();
 
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(ChatBoxActivity.this, "onResume", Toast.LENGTH_SHORT).show();
+        //reload();
+
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(ChatBoxActivity.this, "onRestart", Toast.LENGTH_SHORT).show();
+        //reload();
+
+    }
+
+
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(ChatBoxActivity.this, "onStop", Toast.LENGTH_SHORT).show();
+       // reload();
+
+    }
+
+    public void reload() {
+        Intent intent = getIntent();
+        overridePendingTransition(0, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(intent);
+
+    }
 
 
     @Override
@@ -128,5 +341,8 @@ public class ChatBoxActivity extends AppCompatActivity {
         super.onDestroy();
 
         socket.disconnect();
+        Toast.makeText(ChatBoxActivity.this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        //reload();
     }
+
 }
